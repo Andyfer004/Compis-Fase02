1c4200fab64f6e4e0d3c73e56a86903da1d11c28
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2025-06-02 23:27:54 -0600

    modified main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from parser.evaluator import evaluar_archivo
 
 print("🔎 Variable expressions (Parser 1)")
 evaluar_archivo(
-    "input/variable_expressions.txt",
-    "input/afd_min.pkl",
-    "input/slr-2.yalp"
+    "input/4.txt",
+    "input/afd_min4.pkl",
+    "input/slr-4.yalp"
 )
